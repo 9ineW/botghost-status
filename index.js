832385bfa40be3ca95ea4d8748713d0c574d942b
9ineW <141578777+9ineW@users.noreply.github.com>
2023-08-07 13:38:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let client = new Discord.Client()
 
 
 client.on('ready', () => {
- client.user.setActivity(`1.0.1 your prefix here`, { type: 'PLAYING'})
+ client.user.setActivity(`nigga balls wonkawap`, { type: 'PLAYING'})
 })
 
 client.login(process.env.token);
